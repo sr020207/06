948b166139b181f23d7efc929ae1346268c0ba8a
sr020207 <144992835+sr020207@users.noreply.github.com>
2023-10-12 15:49:14 +0900

    002

--- main.c
+++ main.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int square( int a )
-{
-return (a *a);
+void func(void) {
+int x;
+printf("func1 x is at %p\n", &x);
 }
-
+   
 int main(int argc, char *argv[]) {
+	
+int x;
+
+printf("main x is at %p\n", &x); //%p : 16진수로표현 
+func();
+
+/*
+출력 : 
+main x is at 000000000062FE1C
+func1 x is at 000000000062FDDC
+*/
    
-int a = 2;
-square(a);
-printf("a=%i\n",a);
-   
-   return 0;
+   return 0; 
 }
